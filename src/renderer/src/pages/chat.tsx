cd6101bc687409f0a32d1d763197f2fe8a9c56fc
lybenson <gwanbit@gmail.com>
2024-06-09 15:49:04 +0800

    feat: page

--- src/renderer/src/pages/chat.tsx
+++ src/renderer/src/pages/chat.tsx
@@ -1,12 +1,21 @@
 import { Button } from '@renderer/components/ui/button'
 import { Textarea } from '@renderer/components/ui/textarea'
+import { translate } from '@renderer/translate'
 import { toast } from 'sonner'
 
 export default function Chat() {
+  const handleSubmit = () => {
+    translate('Event has been created.')
+  }
   return (
     <div>
       <Textarea className='min-h-32 mb-4 text-base' />
-      <Button size='sm'>Submit</Button>
+      <Button
+        size='sm'
+        onClick={handleSubmit}
+      >
+        Submit
+      </Button>
       <Button
         size='sm'
         onClick={() => toast.success('Event has been created.')}
